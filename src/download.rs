49df4530acfd82f9295be3d96e914231c189f88f
Blake Loring <blake@parsed.uk>
2018-04-10 19:02:50 +0100

    Substantially cleanup download ctrl code

--- src/download.rs
+++ src/download.rs
@@ -18,16 +18,6 @@ struct Peer {
     channel: (Sender<ClientState>, Receiver<ClientState>)
 }
 
-fn s_peer(peer_list: &mut Vec<Peer>, el: usize, elem: ClientState) {
-    let (ref mut send, _) = peer_list[el].channel;
-    send.send(elem).unwrap();
-}
-
-fn r_peer(peer_list: &mut Vec<Peer>, el: usize) -> Result<ClientState, mpsc::TryRecvError> {
-    let (_, ref mut recv) = peer_list[el].channel;
-    recv.try_recv()
-}
-
 fn remaining(torrent_data: &TorrentData) -> usize {
     (0..torrent_data.pieces.len()).filter(|&x| !torrent_data.have.get(x)).count()
 }
@@ -93,41 +83,53 @@ impl Download {
         }
     }
 
+    fn s_client(&mut self, id: usize, msg: ClientState) {
+        self.active_clients[id].channel.0.send(msg).unwrap();
+    }
+
+    fn r_client(&mut self, id: usize) -> Result<ClientState, mpsc::TryRecvError> {
+        self.active_clients[id].channel.1.try_recv()
+    }
+
+    fn process_client_msg(&mut self, id: usize, msg: ClientState, to_remove: &mut Vec<usize>) {
+        match msg {
+            ClientState::Close(reason) => {
+                println!("Flagging {:?} for close due to {}", self.active_clients[id].id, reason);
+                to_remove.push(id);
+            },
+            ClientState::Need(field) => { 
+                let target = (0..self.data.pieces.len())
+                    .find(|&x| {
+                        //println!("Find {}", x);
+                        let i_have = self.data.have.get(x);
+                        let they_have = field.get(x);
+                        let is_unlocked = remaining(&self.data) < MAX_PEERS || !self.active_clients.iter().any(|cl| cl.locked == Some(x));
+                        !i_have && they_have && is_unlocked
+                    });
+
+                if let Some(i) = target {
+                    self.active_clients[id].locked = Some(i);
+                    self.s_client(id, ClientState::Want(i));
+                } else {
+                    self.s_client(id, ClientState::Close("Nothing of interest".to_string()));
+                }
+            },
+            ClientState::Commit(piece, data) => {
+                println!("{} / {} / {}", piece, self.data.pieces.len(), remaining(&self.data));
+                self.data.write(piece, &data).unwrap();
+            },
+            _ => { println!("TODO: Error handler"); }
+        } 
+    }
+
     pub fn sync_clients(&mut self) {
         //Update peer-wire client info
         let mut closed = Vec::new();
 
         //Read all signals from clients & process
         for client_num in 0..self.active_clients.len() {
-            while let Ok(signal) = r_peer(&mut self.active_clients, client_num) {
-                match signal {
-                    ClientState::Close(reason) => {
-                        println!("Flagging {:?} for close due to {}", self.active_clients[client_num].id, reason);
-                        closed.push(client_num);
-                    },
-                    ClientState::Need(field) => { 
-                        let target = (0..self.data.pieces.len())
-                            .find(|&x| {
-                                //println!("Find {}", x);
-                                let i_have = self.data.have.get(x);
-                                let they_have = field.get(x);
-                                let is_unlocked = remaining(&self.data) < MAX_PEERS || !self.active_clients.iter().any(|cl| cl.locked == Some(x));
-                                !i_have && they_have && is_unlocked
-                            });
-
-                        if let Some(i) = target {
-                            self.active_clients[client_num].locked = Some(i);
-                            s_peer(&mut self.active_clients, client_num, ClientState::Want(i));
-                        } else {
-                            s_peer(&mut self.active_clients, client_num, ClientState::Close("Nothing of interest".to_string()));
-                        }
-                    },
-                    ClientState::Commit(piece, data) => {
-                        println!("{} / {} / {}", piece, self.data.pieces.len(), remaining(&self.data));
-                        self.data.write(piece, &data).unwrap();
-                    },
-                    _ => { println!("TODO: Error handler"); }
-                }       
+            while let Ok(signal) = self.r_client(client_num) {
+                self.process_client_msg(client_num, signal, &mut closed);
             }
         }
 
